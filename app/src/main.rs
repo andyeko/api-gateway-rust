6e6d129c1edc3491648088239e2fc68f2f5f0957
andyeko <andy.eko@gmail.com>
2026-01-31 21:54:22 +0100

    init structure

--- app/src/main.rs
+++ app/src/main.rs
@@ -5,19 +5,12 @@ async fn main() {
     println!("apisentinel app");
 
     let mut handles = Vec::new();
-    let mut any = false;
-
-    #[cfg(feature = "gateway")]
-    {
-        any |= true;
-        handles.push(tokio::spawn(async {
-            modules::gateway::start().await;
-        }));
-    }
+    handles.push(tokio::spawn(async {
+        modules::gateway::start().await;
+    }));
 
     #[cfg(feature = "auth")]
     {
-        any |= true;
         handles.push(tokio::spawn(async {
             modules::auth::start().await;
         }));
@@ -29,11 +22,6 @@ async fn main() {
         handles.push(tokio::spawn(async {
             modules::admin::start().await;
         }));
-    }
-
-    if !any {
-        println!("No modules enabled. Enable features: gateway, auth, admin.");
-        return;
     }
 
     for handle in handles {
